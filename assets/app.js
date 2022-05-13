4884bec06ed2cdbedf62aae696a04e8c71938ae4
narinda giriputra <narinda.giriputra@gmail.com>
2022-05-13 10:37:56 +0800

    change parameter in accordion js

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,14 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+const  acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
+
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active-accordion");
 
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
