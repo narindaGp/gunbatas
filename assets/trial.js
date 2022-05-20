0e03db0696a1d826703e4f50951ef98225a0c872
narindaGp <narinda.giriputra@gmail.com>
2022-05-20 10:05:34 +0800

    trial game

--- assets/trial.js
+++ assets/trial.js
@@ -1,24 +1,29 @@
+const gunbatas = ['Batu', 'Kertas', 'Gunting'];
 class Player {
   constructor(choice, against){
     this.choice = choice;
     this.against = against;
   }
-
+  
   versusCom(){
-    const gunbatas = ['batu', 'kertas', 'gunting'];
     const comChoice = Math.floor(Math.random()*3);
-    this.against = gunbatas[comChoice]   
+    this.against = comChoice   
     
-    console.log(gunbatas[this.choice], 'vs', this.against)
-    if (this.choice == comChoice) {
-      return 'draw'
-    } else if (this.choice - comChoice == 1 || this.choice - comChoice == -2) {
-      return 'win'
-    } else if (this.choice - comChoice == -1 || this.choice - comChoice == 2) {
-      return 'lose'
+    console.log(this.against)
+    
+    const result = [this.against]
+    console.log(gunbatas[this.choice], 'vs', gunbatas[this.against])
+    if (this.choice == this.against) {
+      result.push('draw')
+    } else if (this.choice - this.against == 1 || this.choice - this.against == -2) {
+      result.push('win')
+    } else if (this.choice - this.against == -1 || this.choice - this.against == 2) {
+      result.push('lose')
     } else {
-      return 'error'
+      result.push('error')
     }
+    return result
+    console.log(result)
   }
 }
 
@@ -26,9 +31,37 @@ class Computer extends Player{
 
 }
 
-function suit(choice){
-  const play = new Player(choice);
-  console.log(play.versusCom())
+function matchID(id){
+  if (typeof window !== 'undefined') {
+    return document.getElementById(id)
+  }
 }
 
+function suit(choice){
+  const play = new Player(choice);
+  const [against, outcome] = play.versusCom()
 
+  let playerChoice = matchID(`player${gunbatas[choice]}`)
+  playerChoice.classList.toggle("choiceActive");
+
+  let rivalChoice = matchID(`rival${gunbatas[against]}`)
+  rivalChoice.classList.toggle("choiceActive")
+
+  let suitResult
+  console.log(outcome)
+  matchID('versus').style.display = 'none';
+  matchID('resultDraw').style.display = 'none';
+  matchID('resultWin').style.display = 'none';
+  matchID('resultLose').style.display = 'none';
+  switch (outcome) {
+    case 'draw':
+      matchID('resultDraw').style.display = 'flex';
+      break;
+    case 'win':
+      matchID('resultWin').style.display = 'flex';
+      break;
+    case 'lose':
+      matchID('resultLose').style.display = 'flex';
+      break;
+  }
+}
