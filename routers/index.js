2fee8ee7c3655560956d99cb5a629d5fe97310ee
narindaGp <narinda.giriputra@gmail.com>
2022-05-30 19:05:51 +0800

    add nodemon to local depedencies

--- routers/index.js
+++ routers/index.js
@@ -3,12 +3,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send('Hello World! Gunbatas index routes lalala bla bla bla')
-  });
+});
 
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World! Gunbatas index routes')
-//   })
 
 
 module.exports = router;
