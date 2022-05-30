7cb6a1e1312615e80b1b220ebe0b4a15f7facef5
narindaGp <narinda.giriputra@gmail.com>
2022-05-30 21:10:54 +0800

    render home ejs with css

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 const router = require('./routers')
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/assets'))
 
 
 app.use('/', router)
--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router();
+const Controller = require('../controller')
 
+// router.get('/', (req, res) => {
+//     res.send('Hello World! Gunbatas index routes la bla')
+// });
 router.get('/', (req, res) => {
-    res.send('Hello World! Gunbatas index routes lalala bla bla bla')
+    // res.send('Hello World! Gunbatas index routes la bla')
+    res.render('home')
 });
+
+// router.get('/', Controller.home);
 
 
 
