ef9252b49c445c055a0ca5351dbf72f626800338
narindaGp <narinda.giriputra@gmail.com>
2022-05-31 21:18:28 +0800

    authentication

--- routers/index.js
+++ routers/index.js
@@ -23,8 +23,29 @@ router.get('/login', (req, res) => {
 
 router.post('/login', (req, res) => {
     const users = JSON.parse(fs.readFileSync('./data/user.json', 'utf-8'))
-    console.log(req.body, users)
-    res.send(users)
+    const {email, password} = req.body
+    let isUserEmail = false
+    let isPassword = false
+
+    users.forEach(user => {
+        console.log(user.email, email);
+        if (user.email === email) {
+            isUserEmail = true
+            if (user.password === password) {
+                isPassword = true
+            }
+        }
+    });
+
+    if (isUserEmail === false) {
+        res.send('user not found')
+    } else {
+        if (isPassword === false) {
+            res.send('password incorrect')
+        } else {
+            res.send('login successfully')
+        }
+    }
 })
 
 module.exports = router;
