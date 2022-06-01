91113713ad5409e275dfc2ece4a81504f97a50ad
narindaGp <narinda.giriputra@gmail.com>
2022-06-01 23:29:21 +0800

    navbar conditional

--- routers/index.js
+++ routers/index.js
@@ -4,50 +4,56 @@ const Controller = require('../controller')
 const fs = require('fs')
 
 let isLogin = false
+let playerEmail ='';
 
 router.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', { title: 'Gunbatas homepage', player: playerEmail})
 });
 
 router.get('/trial', (req, res) => {
     if (isLogin) {
-        res.render('trial')      
+        res.render('trial', { title: 'Trial Rock Paper Scissors', player: playerEmail})      
     } else {
         res.render('restricted')
     }
 });
 
 router.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login', { title: 'Login page' })
 });
 
 router.post('/login', (req, res) => {
     const users = JSON.parse(fs.readFileSync('./data/user.json', 'utf-8'))
+
+    // console.log('ini hasil user', users['data'])
+    // res.send(users.data)
     const {email, password} = req.body
     let isUserEmail = false
     let isPassword = false
 
-    users.forEach(user => {
+    users.data.forEach(user => {
         if (user.email === email) {
             isUserEmail = true
         }
         if (isUserEmail === true && user.password === password) {
             isPassword = true
         }
-    });
 
+    });
+    
     if (isUserEmail === false) {
         // res.send('user not found')
-        res.render('restricted')
+        res.render('restricted', { title: 'User not found' })
     } 
     
     if (isPassword === false) {
         // res.send('password incorrect')
-        res.render('restricted')
+        res.render('restricted', { title: 'User not found' })
     } else {
         isLogin = true
         // res.send('login successfully')
-        res.render('home')
+        playerEmail = email 
+        res.render('home', { title: `Gunbatas - ${player = playerEmail}` })
     }
     
 })
